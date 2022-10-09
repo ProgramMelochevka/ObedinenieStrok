64472c6ca59874c1c64dd62326a85cb97e44bef1
Maksim Kushner <113466296+MaxZzzIt@users.noreply.github.com>
2022-10-09 14:56:42 +0300

    Объединение строк

---
+++ src/by/home/maxzzzit/ObedinenieStrok.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package by.home.maxzzzit;
+
+/**
+ * @author Kushner
+ *
+ */
+public class ObedinenieStrok {
+	public static void main(String[] args) {
+		String str = "abc", rezult = "";
+		
+		rezult = str.concat(str);
+		System.out.println(rezult);
+
+	}
+
+}
